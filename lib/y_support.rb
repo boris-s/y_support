9cbeae76e40bab1ed55797bce02526d9d2861f41
boris <boris@iis.sinica.edu.tw>
2012-12-05 15:58:38 +0800

    adapting to y_unicode changes

--- lib/y_support.rb
+++ lib/y_support.rb
@@ -197,8 +197,8 @@ module YSupport
       
       # Applies a block as a mapping on all keys, returning a new hash
       def with_keys
-        keys.each_with_object ç.new do |key, ꜧ|
-          ꜧ[yield k] = self[k]
+        keys.each_with_object ç.new do |hash_key, ꜧ|
+          ꜧ[ yield( hash_key ) ] = self[ hash_key ]
         end
       end
       alias :do_with_keys :with_keys
@@ -206,23 +206,24 @@ module YSupport
       # The difference from do_with_keys is that modify_keys expects block
       # that takes 2 arguments (key: value pair) and returns the new key.
       def modify_keys
-        each_with_object ç.new do |pair, ꜧ|
-          ꜧ[yield pair] = self[pair[0]]
+        each_with_object ç.new do |hash_pair, ꜧ|
+          ꜧ[ yield( hash_pair ) ] = self[ hash_pair[0] ]
         end
       end
       
       # Applies a block as a mapping on all values, returning a new hash
       def with_values
-        each_with_object ç.new do |pair, ꜧ|
-          ꜧ[pair[0]] = yield pair[1]
+        each_with_object ç.new do |hash_pair, ꜧ|
+          ꜧ[ hash_pair[0] ] = yield( hash_pair[1] )
         end
       end
       alias :do_with_values :with_values
       
       # Like #do_with_values, but modifies the receiver.
       def with_values!
-        each_with_object self do |pair, ꜧ|
-          ꜧ[pair[0]] = yield pair[1]
+        each_with_object self do |hash_pair, ꜧ|
+          hash_kay, hash_val = hash_pair
+          ꜧ[ hash_key ] = yield( hash_val )
         end
       end
       alias :do_with_values! :with_values!
@@ -230,22 +231,22 @@ module YSupport
       # The difference from #do_with_values is that modify_values expects block
       # that takes 2 arguments (key: value pair) and returns the new value.
       def modify_values
-        each_with_object ç.new do |pair, ꜧ|
-          ꜧ[pair[0]] = yield pair
+        each_with_object ç.new do |hash_pair, ꜧ|
+          ꜧ[ hash_pair[0] ] = yield( hash_pair )
         end
       end
       
       # Like #modify_values, but modifies the receiver
       def modify_values!
-        each_with_object self do |pair, ꜧ|
-          ꜧ[pair[0]] = yield pair
+        each_with_object self do |hash_pair, ꜧ|
+          ꜧ[ hash_pair[0] ] = yield( hash_pair )
         end
       end
 
       # Like #map that returns a hash.
       def modify
-        each_with_object ç.new do |pair, ꜧ|
-          key, val = yield pair
+        each_with_object ç.new do |hash_pair, ꜧ|
+          key, val = yield hash_pair
           ꜧ[key] = val
         end
       end
