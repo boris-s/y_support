7f6a7863c9a4197e9a88050c596613db3f15c1f8
boris <boris@iis.sinica.edu.tw>
2012-12-16 22:23:46 +0800

    debugging

--- lib/y_support/core_ext/enumerable/support.rb
+++ lib/y_support/core_ext/enumerable/support.rb
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
-class Module
-  # Checks whether #all? are #kind_of? the ç provided as argument
-  def all_kind_of?( ç ); all? {|e| e.kind_of? ç } end
+class Enumerable
+  # Checks whether #all? collection elements are #kind_of? module
+  # supplied as an argument
+  # 
+  def all_kind_of?( kind )
+    all? {|e| e.kind_of? kind }
+  end
   
-  # Checks whether #all? are #kind_of? ::Numeric.
-  def all_numeric?; all? {|e| e.kind_of? Numeric } end
+  # Checks whether #all? collection elements are #kind_of? Numeric.
+  # 
+  def all_numeric?
+    all? {|e| e.kind_of? Numeric }
+  end
   
-  # Checks whether receiver is a "subset" of the argument
-  def subset_of?( other ); all? {|e| other.include? e } end
+  # Checks whether the receiver collection is fully included in the
+  # collection supplied as an argument.
+  # 
+  def subset_of?( other_collection )
+    all? {|e| other_collection.include? e }
+  end
   alias :⊂? :subset_of?
   
-  # Checks whether the argument is a "subset" of the receiver
-  def superset_of?( other ); other.all? {|e| self.include? e } end
+  # Checks whether the receiver collection contains every element of
+  # the collection supplied as an argument.
+  # 
+  def superset_of?( other_collection )
+    other.all? {|e| self.include? e }
+  end
   alias :⊃? :superset_of?
 end
