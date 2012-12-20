d42aa56d1c6eda62b449fb7cb692ff68b5da0092
boris <boris@iis.sinica.edu.tw>
2012-12-21 00:23:04 +0800

    splitting @name_magic_hook into @new_instance_hook and @naming_hook

--- lib/y_support/name_magic.rb
+++ lib/y_support/name_magic.rb
@@ -45,13 +45,16 @@ module NameMagic
     # do nothing if previous name same as the new one
     return if old_ɴ == ɴ
     # otherwise, continue by being cautious about name collisions
-    raise NameError, "#{self.class} instance #{ɴ} already exists!" if
-      self.class.__instances__.rassoc( ɴ )
+    raise NameError, "Name '#{ɴ}' already exists in " +
+      "#{self.class} namespace!" if self.class.__instances__.rassoc( ɴ )
     # if everything's ok., add self to the namespace
     self.class.const_set ɴ, self
-    self.class.__instances__[ self ] = ɴ.to_s
-    # and forget the old name
+    self.class.__instances__[ self ] = ɴ.to_sym
+    # forget the old name
     self.class.forget old_ɴ
+    # and honor the hook
+    naming_hook = self.class.instance_variable_get :@naming_hook
+    naming_hook.call( ◉, const_ß, old_ɴ ) if naming_hook
   end
 
   # Names an instance, aggresively (overwrites existing names).
@@ -67,9 +70,12 @@ module NameMagic
     self.class.forget same_ɴ_inst if same_ɴ_inst
     # add self to the namespace
     self.class.const_set ɴ, self
-    self.class.__instances__[ self ] = ɴ.to_s
-    # and forget the old name
+    self.class.__instances__[ self ] = ɴ.to_sym
+    # forget the old name
     self.class.forget old_ɴ
+    # honor the hook
+    naming_hook = self.class.instance_variable_get :@naming_hook
+    naming_hook.call( ◉, const_ß, old_ɴ ) if naming_hook
     return true
   end
 
@@ -91,7 +97,7 @@ module NameMagic
     def instance which
       const_magic
       return which if ( @instances ||= {} ).keys.include? which
-      inst = @instances.rassoc( which.to_s )
+      inst = @instances.rassoc( which.to_sym )
       raise ArgumentError, "No instance #{which} in #{self}" if inst.nil?
       return inst[0]
     end
@@ -108,36 +114,37 @@ module NameMagic
       ɴς = if oo[:name] then validate_name( oo.delete :name )
            elsif oo[:ɴ] then validate_name( oo.delete :ɴ )
            else nil end
-      # Expecting true/false, if :name_angry option is given
-      aggressive = oo[:name_aggressive] ? oo.delete( :name_aggressive ) : false
-      # Avoid name collisions unless aggressive
+      # Expecting true/false, if :name_avid option is given
+      avid = oo[:name_avid] ? oo.delete( :name_avid ) : false
+      # Avoid name collisions unless avid
       raise NameError, "#{self} instance #{ɴς} already exists!" if
-        ( @instances ||= {} ).keys.include? ɴς unless aggressive
+        ( @instances ||= {} ).keys.include? ɴς unless avid
       # instantiate
       new_inst = if oo.empty? then super *args, &block
                  else super *args, oo, &block end
       # treat is as unnamed at first
       ( @instances ||= {} ).merge! new_inst => nil
-      # and then either name it, if name was supplied, or make it aggressive
-      if ɴς then
-        if aggressive then new_inst.name! ɴς else new_inst.name = ɴς end
-      else
-        ( @name_aggressive_instances ||= [] ) << new_inst
-      end
       # honor the hook
-      @name_magic_hook.call( new_inst ) if @name_magic_hook
+      @new_instance_hook.call( new_inst ) if @new_instance_hook
+      # and then either name it, if name was supplied, or make it avid
+      # (avid instances will replace a competitor, if any, in the name table)
+      if ɴς then
+        if avid then new_inst.name! ɴς else new_inst.name = ɴς end
+      else
+        ( @name_avid_instances ||= [] ) << new_inst
+      end
       # return the new instance
       return new_inst
     end
 
-    # Compared to #new method, #new! uses name_aggressive mode: without
+    # Compared to #new method, #new! uses name_avid mode: without
     # concerns about overwriting existing named instances.
     # 
     def new! *args, &block
       # extract options
       if args[-1].is_a? Hash then oo = args.pop else oo = {} end
-      # and call #new with added name_aggressive: true
-      new *args, oo.merge!( name_aggressive: true )
+      # and call #new with added name_avid: true
+      new *args, oo.merge!( name_avid: true )
     end
 
     # The method will search the namespace for constants, to which the nameless
@@ -167,7 +174,7 @@ module NameMagic
       ɴ = inst.nil? ? nil : inst.name
       send :remove_const, ɴ if ɴ # clear constant assignment
       ( @instances ||= {} ).delete( inst )   # remove @instances entry
-      ( @name_aggressive_instances ||= [] ).delete( inst ) # remove if any
+      ( @name_avid_instances ||= [] ).delete( inst ) # remove if any
       return inst                            # return forgotten instance
     end
 
@@ -176,7 +183,7 @@ module NameMagic
     def forget_anonymous_instances
       nameless_instances.each { |inst, ɴ|
         @instances.delete inst
-        @name_aggressive_instances.delete inst
+        @name_avid_instances.delete inst
       }
     end
     alias :forget_nameless_instances :forget_anonymous_instances
@@ -189,17 +196,24 @@ module NameMagic
         .each { |ß| send :remove_const, ß if const_get( ß ).is_a? self }
     end
     
-    # Registers a hook to execute whenever name magic is performed on a new
-    # instance (in #new method also provided by this module).
+    # Registers a hook to execute whenever name magic creates a new instance
+    # of the class including NameMagic. The block should take one argument
+    # (the new instance that was created) and is called in #new method right
+    # after instantiation, but before naming.
     # 
-    def name_magic_hook &block; @name_magic_hook = block end
+    def new_instance_hook &block; @new_instance_hook = block end
+
+    # Registers a hook to execute whenever naming is performed on an instance.
+    # The block should take three arguments (instance, name, old_name).
+    # 
+    def naming_hook &block; @naming_hook = block end
 
     private
     
     # Checks all the constants in some module's namespace, recursively
     def serve_all_modules
       incriminated_ids = 
-        ( nameless_instances + ( @name_aggressive_instances ||= [] ) )
+        ( nameless_instances + ( @name_avid_instances ||= [] ) )
         .map( &:object_id ).uniq
       ObjectSpace.each_object Module do |ɱ|
         # check all the module constants:
@@ -207,17 +221,18 @@ module NameMagic
           ◉ = ɱ.const_get( const_ß ) rescue nil
           # is it a wanted object?
           if incriminated_ids.include? ◉.object_id then
-            if @name_aggressive_instances.include? ◉ then # name aggressively
-              @name_aggressive_instances.delete ◉
+            if @name_avid_instances.include? ◉ then # name avidly
+              @name_avid_instances.delete ◉
               ◉.name! const_ß
             else # name this anonymous instance cautiously
-              if ( @instances ||= {} )[ ◉ ] or const_get const_ß then
-                raise NameError, "Name '#{const_ß}' already exists in " +
-                  "#{self.class} namespace!"
-              end
+              raise NameError, "Name '#{const_ß}' already exists in " +
+                "#{self.class} namespace!" if
+                  __instances__[ ◉ ] || const_get const_ß
               # if everything's ok., add the instance to the namespace
-              @instances[ ◉ ] = const_ß.to_s
+              __instances__[ ◉ ] = const_ß
               const_set const_ß, ◉
+              # honor naming_hook
+              @naming_hook.call( ◉, const_ß, nil ) if @naming_hook
             end
             # and stop working in case there are no more unnamed instances
             incriminated_ids.delete ◉.object_id
