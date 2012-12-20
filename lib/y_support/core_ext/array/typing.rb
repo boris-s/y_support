20898d38a2b5fa27d0e299a64a3ef692ccb43395
boris <boris@iis.sinica.edu.tw>
2012-12-20 23:14:41 +0800

    Removal of USE_SCRUPLE and my beloved Scruple Unicode character (℈).

--- lib/y_support/core_ext/array/typing.rb
+++ lib/y_support/core_ext/array/typing.rb
@@ -4,9 +4,6 @@ class Array
   def aE_has( e, msg = "Element #{e} absent from the array" )
     raise AE, msg unless self.include? e
     return self end
-  alias :a℈_has :aE_has if ::YSupport::USE_SCRUPLE
   alias :aE_include :aE_has
-  alias :a℈_include :aE_has if ::YSupport::USE_SCRUPLE
-  alias :aE_∋ :aE_has if ::YSupport::USE_SCRUPLE
-  alias :a℈_∋ :aE_has if ::YSupport::USE_SCRUPLE
+  alias :aE_∋ :aE_has
 end
