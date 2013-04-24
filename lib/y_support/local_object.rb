d1b2cee72803516c807c4376bf4ca0134fc89e42
boris <boris@iis.sinica.edu.tw>
2013-04-24 23:09:05 +0800

    trying to make LocalObject better

--- lib/y_support/local_object.rb
+++ lib/y_support/local_object.rb
@@ -4,7 +4,7 @@ require 'y_support'
 
 # Object, whose business is to stay local to methods. Optional signature
 # provides additional level of safety in ensuring object locality. (Signature
-# accessor is :signature, aliased as :σ (small Greek sigma).)
+# accessor is :signature, aliased as :σ, small Greek sigma.)
 # 
 class LocalObject
   attr_reader :signature
@@ -13,23 +13,27 @@ class LocalObject
   # Optional argument signature provides additional level of safety in
   # ascertaining that the object indeed is of local origin.
   # 
-  def initialize signature=__callee__
-    @signature=signature
+  def initialize signature=caller_locations( 1, 1 )[0].label
+    @signature = signature
   end
 
   # True if the (optional) signature matches.
   # 
-  def local_object? signature=__callee__
+  def local_object? signature=caller_locations( 1, 1 )[0].label
     signature == self.signature
   end
   alias ℓ? local_object?
 end
 
-
+# Object class is patched with #LocalObject (alias L!) constructor, and
+# #local_object?, alias #ℓ? inquirer.
+# 
 class Object
   # LocalObject constructor.
   # 
-  def LocalObject signature=nil; LocalObject.new signature end
+  def LocalObject signature=caller_locations( 1, 1 )[0].label
+    LocalObject.new signature
+  end
   alias L! LocalObject
 
   # False for normal objects, overriden in the LocalObject class.
