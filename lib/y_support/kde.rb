aae36b5089aa9cc68d6e698ceeeeb4962022b41c
boris <boris@iis.sinica.edu.tw>
2013-07-02 08:28:56 +0800

    transfer of GUI dialog box methods into this library

--- lib/y_support/kde.rb
+++ lib/y_support/kde.rb
@@ -3,9 +3,57 @@ require 'y_support'
 
 module YSupport::KDE
   class << self
-    # display it with kioclient
+    # Open a file with kioclient (using KDE file-viewer associations).
+    # 
     def show_file_with_kioclient( fɴ_with_path )
       system "sleep 0.2; kioclient exec 'file:%s'" % fɴ_with_path
+    end
+
+    # Dialog box querying for a string.
+    # 
+    def query_box( dialog_text, prompt: '> ', title_bar: 'User input required', &block )
+      title_bar_text = title_bar
+      prompt_label_text = prompt
+      
+      w = Gtk::Window.new( Gtk::Window::TOPLEVEL )
+      w.set_title( title_bar_text )
+      w.border_width = 10
+      w.signal_connect 'delete_event' do Gtk.main_quit end               # cc
+      
+      tlabel_widget = Gtk::Label.new( dialog_text )
+      plabel_widget = Gtk::Label.new( prompt_label_text )
+      ebox_widget = Gtk::Entry.new
+      ebox_widget.visibility = true                                      # cc
+      
+      hbox = Gtk::HBox.new(false, 5)                                     # cc
+      hbox.pack_start_defaults( plabel_widget )                          # cc
+      hbox.pack_start_defaults( ebox_widget )
+      vbox = Gtk::VBox.new(false, 5)
+      vbox.pack_start_defaults( tlabel_widget )
+      vbox.pack_start_defaults( hbox )
+      w.add(vbox)
+      
+      ebox_widget.signal_connect("key-release-event") do |sender, event| # cc
+        kn = Gdk::Keyval.to_name(k = event.keyval)
+        if kn == "Return"
+          block.( sender.text )
+          Gtk.main_quit
+        end
+      end
+
+      w.show_all                                                         # cc
+      Gtk.main
+    end
+
+    # Message box.
+    # 
+    def message_box( message="Press any key to close this window!" )
+      w = Gtk::Window.new
+      w.add_events Gdk::Event::KEY_PRESS
+      w.add Gtk::Label.new( message )
+      w.signal_connect "key-release-event" do Gtk.main_quit end
+      w.set_default_size( 600, 120 ).show_all
+      Gtk.main
     end
   end
 end # module YSupport::KDE
--- test/kde_test.rb
+++ test/kde_test.rb
@@ -1,17 +1,14 @@
 #! /usr/bin/ruby
 #encoding: utf-8
 
-require 'test/unit'
-require 'shoulda'
+require 'minitest/spec'
+require 'minitest/autorun'
+require './../lib/y_support/kde'
 
-class KDETest < Test::Unit::TestCase
-  context "KDE" do
-    setup do
-      require 'y_support/kde'
-    end
-
-    should "have module method #show_file_with_kioclient" do
-      assert_respond_to YSupport::KDE, :show_file_with_kioclient
-    end
-  end # context KDE
-end # class KDETest
+describe YSupport::KDE do
+  it "should at least respond to certain messages" do
+    assert_respond_to YSupport::KDE, :show_file_with_kioclient
+    assert_respond_to YSupport::KDE, :query_box
+    assert_respond_to YSupport::KDE, :message_box
+  end
+end
