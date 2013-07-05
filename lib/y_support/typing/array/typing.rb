dfa5ab57a6ea4873dc4db05366b308a6ec28e627
boris <boris@iis.sinica.edu.tw>
2013-07-06 07:23:25 +0800

    Adding Array#aT_uniq to typing

--- lib/y_support/typing/array/typing.rb
+++ lib/y_support/typing/array/typing.rb
@@ -6,12 +6,18 @@ class Array
   # fails to include the specified element. An optional argument customizes the
   # error message (element description).
   # 
-  def aT_includes element, what_is_element=nil
+  def aT_includes element, what_is_self="array", what_is_element=nil
     e = what_is_element ? what_is_element.to_s.capitalize :
       "Element (#{element.class} instance)"
-    m = "#{e} is absent from the array."
-    raise TErr, m unless include? element
-    return self
+    m = "#{e} is absent from #{what_is_self}!"
+    tap { include? element or fail TypeError, m }
   end
   alias :aT_include :aT_includes
+
+  # Fails with TypeError if the array contains duplicates (using +#uniq+).
+  # 
+  def aT_uniq what_is_self="array"
+    m = "#{what_is_self.to_s.capitalize} non-uniq!"
+    tap { self == uniq or fail TypeError, m }
+  end
 end
