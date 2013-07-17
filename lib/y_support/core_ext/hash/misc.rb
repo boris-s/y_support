b0c6abf2feda5e5881da6f476bbfdeb29a3b9bfe
boris <boris@iis.sinica.edu.tw>
2013-07-17 17:34:46 +0800

    very minor refactor of hash/misc.rb

--- lib/y_support/core_ext/hash/misc.rb
+++ lib/y_support/core_ext/hash/misc.rb
@@ -6,48 +6,46 @@ class Hash
   # reversed merge!: defaults.merge( self! )
   alias :default! :reverse_merge!
   
-  # Applies a block as a mapping on all keys, returning a new hash
+  # Applies a block as a mapping on all keys, returning a new hash.
+  # 
   def with_keys
-    keys.each_with_object self.class.new do |hash_key, ꜧ|
-      ꜧ[ yield( hash_key ) ] = self[ hash_key ]
+    keys.each_with_object self.class.new do |hash_key, hsh|
+      hsh[ yield hash_key ] = self[ hash_key ]
     end
   end
-  alias :do_with_keys :with_keys
   
-  # The difference from do_with_keys is that modify_keys expects block
-  # that takes 2 arguments (key: value pair) and returns the new key.
+  # The difference from #with_keys is that modify_keys expects block that takes
+  # 2 arguments (key: value pair) and returns the new key.
+  # 
   def modify_keys
-    each_with_object self.class.new do |hash_pair, ꜧ|
-      ꜧ[ yield( hash_pair ) ] = self[ hash_pair[0] ]
+    each_with_object self.class.new do |hash_pair, hsh|
+      hsh[ yield( hash_pair ) ] = self[ hash_pair[0] ]
     end
   end
   
-  # Applies a block as a mapping on all values, returning a new hash
+  # Applies a block as a mapping on all values, returning a new hash.
+  # 
   def with_values
-    each_with_object self.class.new do |hash_pair, ꜧ|
-      ꜧ[ hash_pair[0] ] = yield( hash_pair[1] )
-    end
+    each_with_object self.class.new do |(k, v), hsh| hsh[ k ] = yield v end
   end
-  alias :do_with_values :with_values
   
   # Like #do_with_values, but modifies the receiver.
+  # 
   def with_values!
-    each_with_object self do |hash_pair, ꜧ|
-      hash_key, hash_val = hash_pair
-      ꜧ[ hash_key ] = yield( hash_val )
-    end
+    each_with_object self do |(k, v), hsh| hsh[ k ] = yield v end
   end
-  alias :do_with_values! :with_values!
   
   # The difference from #do_with_values is that modify_values expects block
   # that takes 2 arguments (key: value pair) and returns the new value.
+  # 
   def modify_values
     each_with_object self.class.new do |hash_pair, ꜧ|
       ꜧ[ hash_pair[0] ] = yield( hash_pair )
     end
   end
   
-  # Like #modify_values, but modifies the receiver
+  # Like #modify_values, but modifies the receiver.
+  # 
   def modify_values!
     each_with_object self do |hash_pair, ꜧ|
       ꜧ[ hash_pair[0] ] = yield( hash_pair )
@@ -55,6 +53,7 @@ class Hash
   end
   
   # Like #map that returns a hash.
+  # 
   def modify
     each_with_object self.class.new do |hash_pair, ꜧ|
       key, val = yield hash_pair
@@ -76,24 +75,15 @@ class Hash
   # its methods, ArgumentError is raised, unless :overwrite_methods
   # option == true.
   # 
-  def dot!( oo = {} )
+  def dot! overwrite_methods: false
     keys.each do |key|
       msg = "key #{key} of #dot!-ted hash is not convertible to a symbol"
-      raise ArgumentError, msg unless key.respond_to? :to_sym
-      unless oo[:overwrite_methods]
-        if methods.include? key.to_sym
-          raise ArgumentError, "#dot!-ted hash must not have key names " +
-            "colliding with its methods"
-        end
-      end
-      
-      define_singleton_method key.to_sym do
-        self[key]
-      end
-      
-      define_singleton_method "#{key}=".to_sym do |value|
-        self[key] = value
-      end
+      fail ArgumentError, msg unless key.respond_to? :to_sym
+      msg = "#dot!-ted hash must not have key names colliding with its methods"
+      fail ArgumentError, msg if methods.include? key.to_sym unless
+        overwrite_methods
+      define_singleton_method key.to_sym do self[key] end
+      define_singleton_method "#{key}=".to_sym do |value| self[key] = value end
     end
     return self
   end
