e4744ac0b99ceee560398e5a46beadb981123c82
boris <boris@iis.sinica.edu.tw>
2013-07-25 19:10:37 +0800

    Class#parametrize

---
+++ lib/y_support/core_ext/class.rb
@@ -0,0 +1,2 @@
+require 'y_support'
+require 'y_support/core_ext/class/misc'
---
+++ lib/y_support/core_ext/class/misc.rb
@@ -0,0 +1,16 @@
+#encoding: utf-8
+
+class Class
+  # Creates a subclass of the current class parametrized with a given set of
+  # parameters. The parameters have form { symbol: value } and they cause
+  # singleton method(s) named "symbol" be defined on the subclass, returning
+  # "value".
+  # 
+  def parametrize **parameters
+    Class.new( self ).tap do |subclass|
+      parameters.each_pair { |symbol, value|
+        subclass.define_singleton_method symbol do value end
+      }
+    end
+  end
+end
