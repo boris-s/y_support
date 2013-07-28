581125e6a59f7e7f4b75ccb280d5217fe539d44b
boris <boris@iis.sinica.edu.tw>
2013-07-28 08:11:06 +0800

    + banged versions of Object#set_attr_with_readers and Object#param_class

--- lib/y_support/core_ext/object/misc.rb
+++ lib/y_support/core_ext/object/misc.rb
@@ -2,24 +2,46 @@
 
 class Object
   # Assigns prescribed atrributes to the object and makes them accessible with
-  # getter (reader) methods. Optional argument +:overwrite_methods+ enables the
-  # readers to overwrite existing methods.
+  # getter (reader) methods. Raises NameError should any of the getters shadow /
+  # overwrite existing methods.
   # 
-  def set_attr_with_readers( hash )
+  def set_attr_with_readers hash
+    hash.each_pair { |ß, value|
+      fail NameError, "Method \##{ß} already defined!" if methods.include? ß
+      set_attr_with_readers! ß => value
+    }
+  end
+
+  # Assigns prescribed atrributes to the object and makes them accessible with
+  # getter (reader) methods. Shadows / overwrites existing methods.
+  # 
+  def set_attr_with_readers! hash
     hash.each_pair { |symbol, value|
       instance_variable_set "@#{symbol}", value
-      fail NameError, "Method \##{symbol} already defined!" if
-        methods.include? symbol unless hash[:overwrite_methods] == true
       singleton_class.class_exec { attr_reader symbol }
     }
   end
 
-  # Makes the receiver own parametrized subclasses of the supplied classes.
-  # Expects a hash of pairs { reader_symbol: class }, and a hash of parameters,
-  # with which the class(es) is (are) parametrized. Parametrized subclasses
-  # are made accessible under the supplied reader symbol.
+  # Constructs parametrized subclasses of the supplied classes and makes them
+  # available under specified getters. Expects a hash of pairs { reader_symbol:
+  # class }, and a hash of parameters, with which the class(es) is (are)
+  # parametrized. Raises NameError should any of the getters shadow / overwrite
+  # existing methods.
   # 
-  def param_class hash, with: (fail ArgumentError, "No parameters!"), **nn
+  def param_class hash, with: (fail ArgumentError, "No parameters!")
+    hash.each { |ß, ç|
+      sub = ç.parametrize with
+      set_attr_with_readers( ß => sub )
+    }
+    return nil
+  end
+
+  # Constructs parametrized subclasses of the supplied classes and makes them
+  # available under specified getters. Expects a hash of pairs { reader_symbol:
+  # class }, and a hash of parameters, with which the class(es) is (are)
+  # parametrized. Shadows / overwrites existing methods.
+  # 
+  def param_class hash, with: (fail ArgumentError, "No parameters!")
     hash.each { |ß, ç|
       sub = ç.parametrize with
       set_attr_with_readers( ß => sub, **nn )
--- lib/y_support/version.rb
+++ lib/y_support/version.rb
@@ -1,3 +1,3 @@
 module YSupport
-  VERSION = "2.0.26"
+  VERSION = "2.0.27"
 end
