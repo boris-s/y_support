a1871b22df9385d48d0d265b3c44fa81a380ef9e
boris <boris@iis.sinica.edu.tw>
2013-08-03 08:59:20 +0800

    Module#★ alias for Module#include, Numeric#√, Numeric#sqrt, and removal of the redundant aliases of #Σ, #Π, and #ⓒ

--- lib/y_support/unicode.rb
+++ lib/y_support/unicode.rb
@@ -2,15 +2,76 @@
 
 require 'y_support'
 
-# This library sets forth 4 standard abbreviations of Ruby keywords:
+# A tiny number of Unicode 1-character aliases:
 #
-# * <b>ç</b> – class (c with cedilla, U00E7, compose seq. [c, comma])
-# * <b>ⓒ</b> – singleton class (copyright sign, U00A9, compose seq. [(, c, )])
-# * <b>λ</b> – lambda (Greek character lambda)
-# * <b>Λ</b> – proc (Greek character capital lambda)
+# * <b>ç</b> – class (c with cedilla, U00E7)
+# * <b>ⓒ</b> – singleton class (copyright sign)
+# * <b>√</b> – square root
+# * <b>Σ</b> – summation
+# * <b>Π</b> – product
 #
-# It is also encouraged that other on-letter Unicode abbreviations are
-# used especially for local variables:
+class Object
+  alias :ç :class
+  alias ⓒ singleton_class
+
+  # Sum. The argument is expected to be a collection; block can be specified.
+  # Basically same as chaining .reduce( :+ ) to the end; Σ() notation can be
+  # more readable at times.
+  # 
+  def Σ( collection )
+    collection.reduce { |acc, element|
+      acc + ( block_given? ? yield( element ) : element )
+    }
+  end
+
+  # Product. The argument is expected to be a collection; block can be specified.
+  # Basically same as chaining .reduce( :* ) to the end; Π() notation can be
+  # more readable at times.
+  # 
+  def Π( collection )
+    collection.reduce { |acc, element|
+      acc * ( block_given? ? yield( element ) : element )
+    }
+  end
+end
+
+
+# Defined aliases:
+# 
+# * <b>★</b> – alias for include
+# * <b>ç</b> for +class+ in several method names
+# 
+class Module
+  alias ★ include
+  
+  alias ç_variable_set class_variable_set
+  alias ç_variable_get class_variable_get
+  alias ç_variable_defined? class_variable_defined?
+  alias remove_ç_variable remove_class_variable
+end
+
+
+
+# Defined aliases
+# 
+# * <b>√</b> – root of arbitrary degree ( x.√( n ) is n ** ( 1 / x ) ).
+# * <b>sqrt</b> as in +4.sqrt+ for square root (Math#sqrt).
+#
+class Numeric
+  def √ number
+    number ** ( 1 / self )
+  end
+
+
+  # Square root (using Math#sqrt).
+  # 
+  def sqrt
+    Math.sqrt( self )
+  end
+end
+
+
+# Other abbreviations (eg. for local variables) are optionally encouraged:
 # 
 # * <b>ɱ</b> – module (m with hook, U2C6E, compose seq. [m, j])
 # * <b>ꜧ</b> – hash (latin small letter heng, UA727, compose seq. [h, j])
@@ -20,52 +81,10 @@ require 'y_support'
 # * <b>w</b> – abbreviation for "with"
 # * <b>wo</b> – abbreviation for "without"
 # 
-# There are, however, no defined methods using these in YSupport. In other
-# words, using these additional abbreviations is completely up to the goodwill
-# of the developer.
-#
-# ==== Note on compose sequences
+# Note on compose sequences:
 # 
-# Each compose sequence has to be preceded by pressing the <compose> key.
-# The compose sequences comply with the standard Kragen's .XCompose file
-# (https://github.com/kragen/xcompose). In some cases, the needed characters
-# are not in Kragen's file and need to be defined manually.
-# 
-class Object
-  alias :ç :class
-  alias :ⓒ :singleton_class
-  alias :© :singleton_class
-
-  # Square root (proxy for Math.sqrt(x)).
-  # 
-  def √( number ); Math.sqrt( number ) end
-
-  # Sum. The argument is expected to be a collection; block can be specified.
-  # Basically same as chaining .reduce( :+ ) to the end; Σ() notation can be
-  # more readable at times.
-  # 
-  def ∑( collection )
-    collection.reduce { |acc, element|
-      acc + ( block_given? ? yield( element ) : element )
-    }
-  end
-  alias :Σ :∑
-
-  # Product. The argument is expected to be a collection; block can be specified.
-  # Basically same as chaining .reduce( :* ) to the end; Π() notation can be
-  # more readable at times.
-  # 
-  def ∏( collection )
-    collection.reduce { |acc, element|
-      acc * ( block_given? ? yield( element ) : element )
-    }
-  end
-  alias :Π :∏
-end
-
-class Module
-  alias :ç_variable_set :class_variable_set
-  alias :ç_variable_get :class_variable_get
-  alias :ç_variable_defined? :class_variable_defined?
-  alias :remove_ç_variable :remove_class_variable
-end
+# Each compose sequence is preceded the <compose> key, whose location depends
+# on your system configuration. The sequences above comply with the standard
+# Kragen's .XCompose file (https://github.com/kragen/xcompose). In some cases,
+# the characters not in Kragen's file have to be defined manually for
+# comfortable typing.
--- test/unicode_test.rb
+++ test/unicode_test.rb
@@ -1,33 +1,30 @@
 #! /usr/bin/ruby
 #encoding: utf-8
 
-require 'test/unit'
-require 'shoulda'
-# require 'y_support/all'
+require 'minitest/spec'
+require 'minitest/autorun'
 
-class UnicodeTest < Test::Unit::TestCase
-  context "Object" do
-    setup do
-      require 'y_support/unicode'
-    end
+describe "y_support/unicode" do
+  before do
+    require 'y_support/unicode'
+  end
 
-    should "have a very limited number of one-character Unicode aliases" do
-      ◉ = Object.new
-      assert ◉.singleton_class == ◉.©
-      assert ◉.singleton_class == ◉.ⓒ
-      assert ◉.ç == ◉.class
-      assert 2 == √( 4 )
-      assert 10 == ∑(1..4)
-      assert 10 == Σ(1..4)
-      assert 24 == ∏(1..4)
-      assert 24 == Π(1..4)
-      ɱ = Module.new
-      ɱ.ç_variable_set :@@meaning, 42
-      assert ɱ.class_variable_get( :@@meaning ) == 42
-      assert ɱ.ç_variable_get( :@@meaning ) == 42
-      assert ɱ.ç_variable_defined?( :@@meaning )
-      ɱ.remove_ç_variable :@@meaning
-      assert ! ɱ.ç_variable_defined?( :@@meaning )
-    end
+  it "should define a tiny number of Unicode aliases" do
+    o = Object.new
+    assert o.singleton_class == o.ⓒ
+    assert o.ç == o.class
+    assert 2 == √( 4 )
+    assert 10 == Σ(1..4)
+    assert 24 == Π(1..4)
+    2.must_equal 3.√( 8 )
+    ɱ = Module.new
+    ɱ.ç_variable_set :@@meaning, 42
+    assert ɱ.class_variable_get( :@@meaning ) == 42
+    assert ɱ.ç_variable_get( :@@meaning ) == 42
+    assert ɱ.ç_variable_defined?( :@@meaning )
+    ɱ.remove_ç_variable :@@meaning
+    assert ! ɱ.ç_variable_defined?( :@@meaning )
+    ɱ.module_exec { ★ Comparable }
+    assert ɱ < Comparable
   end
 end
