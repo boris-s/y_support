abd368c6bf143d90559b2d4df1964a0886f37832
boris <boris@iis.sinica.edu.tw>
2014-02-22 14:16:32 +0800

    Adding Module#map! method

--- lib/y_support/core_ext/module/misc.rb
+++ lib/y_support/core_ext/module/misc.rb
@@ -13,4 +13,16 @@ class Module
     send :remove_const, const if const_defined? const
     const_set( const, value )
   end
+
+  # Defines a set of methods by applying the block on the return value of
+  # another set of methods. Accepts a hash of pairs { mapped_method_symbol =>
+  # original_method_symbol } and a block with which to perform mapping.
+  # 
+  def map! **hash, &block
+    hash.each_pair { |mapped_method_symbol, original_method_symbol|
+      define_method mapped_method_symbol do |*args, &b|
+        block.( send original_method_symbol, *args, &b )
+      end
+    }
+  end
 end
