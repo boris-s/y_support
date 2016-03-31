05feda80d4d98b8ca1d8cd789812c5a19e189f61
Boris Stitnicky <boris@iis.sinica.edu.tw>
2016-03-31 12:04:28 +0200

    Removing redundant alias definition

--- lib/y_support/core_ext/class/misc.rb
+++ lib/y_support/core_ext/class/misc.rb
@@ -1,14 +1,6 @@
 require File.dirname( __FILE__ ) + '/../module'
 
 class Class
-  # Paper "The mechanical evaluation of expressions" by Landin, 1964, is a
-  # seminal paper formally describing object-oriented languages for the first
-  # time. It describes objects as having, among others, three operation defined
-  # above them: predicates, selectors and constructors. I am hereby allowing to
-  # use historical alias "selector" for Class#attr_reader method.
-  #
-  alias selector attr_reader
-  
   # Creates a subclass of the current class parametrized with a given set of
   # parameters. The parameters have form { symbol: value } and they cause
   # singleton method(s) named "symbol" be defined on the subclass, returning
