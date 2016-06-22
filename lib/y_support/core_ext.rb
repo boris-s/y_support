2002cd212e2a05474322211475f8a8f622f167ae
Boris Stitnicky <boris@iis.sinica.edu.tw>
2016-06-22 19:22:48 +0200

    Adding pro forma file core_ext.rb (a copy of misc_test.rb).

--- lib/y_support/core_ext.rb
+++ lib/y_support/core_ext.rb
@@ -1,3 +1,9 @@
-Dir["#{File.dirname( __FILE__ )}/core_ext/*.rb"].sort.each do |path|
-  require_relative "y_support/core_ext/#{File.basename( path, '.rb' )}"
-end
+require_relative 'core_ext/array'
+require_relative 'core_ext/class'
+require_relative 'core_ext/enumerable'
+require_relative 'core_ext/hash'
+require_relative 'core_ext/module'
+require_relative 'core_ext/numeric'
+require_relative 'core_ext/object'
+require_relative 'core_ext/string'
+require_relative 'core_ext/symbol'
--- lib/y_support/version.rb
+++ lib/y_support/version.rb
@@ -1,4 +1,4 @@
 module YSupport
-  VERSION = "2.4.4"
+  VERSION = "2.4.5"
   DEBUG = false
 end
